ca5e4d3fbc34531d1990169520b434a4f7528b02
knights1210 <jamis.1984@gmail.com>
2021-07-30 17:26:04 +0900

    ラグオリブログ準備中

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,13 +1,22 @@
 import Styles from "../styles/Body.module.css";
+import Image from "next/image";
 
 export default function Body() {
   return (
     <>
       <div className={Styles.container}>
         <h2 className={Styles.title}>TITLE</h2>
-        <hr />
-        <p className={Styles.text}>text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章</p>
-    </div>
+        <hr className={Styles.hr} />
+        <p className={Styles.text}>
+          text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章text文章
+         
+        </p>
+      </div>
+      <style jsx>{`
+        hr {
+          background-color: red;
+        }
+      `}</style>
     </>
-   )
- }
+  );
+}
--- components/thumbnail.jsx
+++ components/thumbnail.jsx
@@ -1,9 +1,17 @@
-import Styles from "../styles/Thumbnail.module.css"
+import Styles from "../styles/Thumbnail.module.css";
+import Image from "next/image";
 
 export default function Thumbnail() {
   return (
     <>
       <div className={Styles.thumbnail}>
+        <Image
+          src="/top.png"
+          width={1920}
+          height={500}
+          objectFit="cover"
+          alt=""
+        />
         <h1 className={Styles.title}>TITLE</h1>
       </div>
     </>
